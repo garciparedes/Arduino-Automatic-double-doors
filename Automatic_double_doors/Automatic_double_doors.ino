7396482ff10417573e8be279637d45e6c90b1f0e
Sergio García <garciparedes@gmail.com>
2014-08-19 15:04:38 +0200

    Inicialize project

---
+++ Automatic_double_doors/Automatic_double_doors.ino
@@ -0,0 +1,23 @@
+/*
+
+
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+ 
+  
+  
+}
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  
+  
+}
